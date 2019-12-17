cc6f0f11f529316cffc7e4282694a2d8d15566ca
Siavash Khalvati <siavash.khalvati@mydrivesolutions.com>
2019-12-17 20:10:06 +0000

    clean up

--- app.py
+++ app.py
@@ -5,95 +5,17 @@ import logging
 import slack
 import ssl as ssl_lib
 import certifi
-from scheduled_message import ScheduledMessage
-from messaging import Communication
-
-# For simplicity we'll store our app data in-memory with the following data structure.
-# scheduled_message_sent = {"channel": {"user_id": OnboardingTutorial}}
-message_sent = {}
-scheduled_message_sent = {}
-def greeting(web_client: slack.WebClient, user_id: str, channel: str):
-    # Create a new communication.
-    communication = Communication(channel)
-    
-    print ("first_communication is: \n", dir(communication))
-    # Get the onboarding e payload
-    message = communication.get_message_payload()
-    print ("First_message is: \n", message)
-    # Post the onboarding message in Slack
-    response = web_client.chat_postMessage(**message)
-
-    # Capture the timestamp of the message we've just posted so
-    # we can use it to update the message after a user
-    # has completed an onboarding task.
-    communication.timestamp = response["ts"]
-    print ("TIMESTAMP is: \n", communication.timestamp)
-
-    # Store the message sent in messaging_sent
-    if channel not in message_sent:
-        message_sent[channel] = {}
-    message_sent[channel][user_id] = communication
-
-    print ("messaege_sent is: \n", message_sent)
-
-def time_deference(start_time, end_time=time.time()):
-    return (end_time - start_time)/3600
-
-def file_modified_time(path_to_file):
-    stat = os.stat(path_to_file)
-    return stat.st_mtime
+import message
 
 
-def schdule_a_message(web_client: slack.WebClient, user_id: str, channel: str, message_arrived_at: int , usre_id: str):
-    # Create a new onboarding tutorial.
-    
-    communication = Communication(channel)
-    first_message = communication.get_message_payload()
-    message1_timestamp = int(message_arrived_at + 20)
-    text_message = "Greetings"
-    extra_elements = {'post_at': message1_timestamp, 'text': text_message}
-    first_message.update(extra_elements)
-    scheduled_message = ScheduledMessage(channel)
-
-    # Get the onboarding message payload
-    message = scheduled_message.get_message_payload()
-    # print ("second Message is: \n",first_message)
-    # Post the onboarding message in Slack
-    response = web_client.chat_scheduleMessage(**first_message)
-
-    # Capture the timestamp of the message we've just posted so
-    # we can use it to update the message after a user
-    # has completed an onboarding task.
-    scheduled_message.timestamp = response["ts"]
-    # print ("TIMESTAMP is: \n", scheduled_message.timestamp)
-    # print ("Response is: \n", response)
-
-    # Store the message sent in scheduled_message_sent
-    if channel not in scheduled_message_sent:
-        scheduled_message_sent[channel] = {}
-    scheduled_message_sent[channel][user_id] = scheduled_message
-
-
-# ============== Message Events ============= #
-# When a user sends a DM, the event type will be 'message'.
-# Here we'll link the message callback to the 'message' event.
 @slack.RTMClient.run_on(event="message")
-def message(**payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
-    """
-    print ("the data is: \n", payload)
-    data = payload["data"]
-    web_client = payload["web_client"]
-    channel_id = data.get("channel")
-    user_id = data.get("user")
-    text = data.get("text")
-    message_ts = float(data.get("ts"))
 
-    if text and (text.lower() == "morning" or text.lower() == "good morning"):
-        return schdule_a_message(web_client, user_id, channel_id, message_ts, user_id)
-    if text and text.lower() == "goodbye":
-        rtm_client.stop()
+def handle_message(**payload):
+    """Passing the message payload to message_handler
+    .
+    """
+    handler = message.MessageHandler(payload)
+    handler.handle()
 
 
 if __name__ == "__main__":
@@ -103,4 +25,9 @@ if __name__ == "__main__":
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
     slack_token = os.environ["SLACK_BOT_TOKEN"]
     rtm_client = slack.RTMClient(token=slack_token, ssl=ssl_context)
+
+# TO DO:
+# { Find another way to get the bot's information
+    BOT_INFORMARTION = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN']).auth_test()
+# }
     rtm_client.start()
